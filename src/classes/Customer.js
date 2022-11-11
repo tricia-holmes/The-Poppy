d81e267373d84d5b57354418d96614b261cb1d5c
Tricia Holmes <tricia.michele.holmes@gmail.com>
2022-11-11 13:29:40 -0600

    Add booking method, create instances of booking, and pass tests

--- src/classes/Customer.js
+++ src/classes/Customer.js
@@ -1,40 +1,49 @@
+import Booking from './Booking'
+
 class Customer {
   constructor(customerData = {}, bookings = []) {
-    this.id = customerData.id, 
-    this.name = customerData.name,
+    this.id = customerData.id
+    this.name = customerData.name
     this.bookings = bookings
   }
 
   static fromCustomerData(customerData, bookingData) {
-    const matchedBookings = bookingData.filter(
-      (booking) => booking.userID === customerData.id)
-      
+    const matchedBookings = bookingData
+      .filter((booking) => booking.userID === customerData.id)
+      .map((booking) => new Booking(booking))
+
     return new Customer(customerData, matchedBookings)
   }
 
   showPastBookings() {
-    const newDate = new Date()
-    const currentDate = `${newDate.getFullYear()}/${newDate.getMonth()+1}/${newDate.getDate()}`
-    return this.bookings.filter(booking => booking.date < currentDate)  
+    const newDate = new Date() // change this to be a global variable passed in
+    const currentDate = `${newDate.getFullYear()}/${
+      newDate.getMonth() + 1
+    }/${newDate.getDate()}`
+    return this.bookings.filter((booking) => booking.date < currentDate)
   }
 
   showUpcomingBookings() {
-    const newDate = new Date()
-    const currentDate = `${newDate.getFullYear()}/${newDate.getMonth()+1}/${newDate.getDate()}`
-    return this.bookings.filter(booking => booking.date >= currentDate)  
+    const newDate = new Date() // change this to be a global variable passed in
+    const currentDate = `${newDate.getFullYear()}/${
+      newDate.getMonth() + 1
+    }/${newDate.getDate()}`
+    return this.bookings.filter((booking) => booking.date >= currentDate)
   }
 
-  getTotalCost(roomData) {
+  getTotalCost(hotel) {
     return this.bookings.reduce((total, booking) => {
-      roomData.forEach(roomData => {
-        if (booking.roomNumber === roomData.number) {
-          total += roomData.costPerNight
+      hotel.rooms.forEach((hotelRoom) => {
+        if (booking.roomNumber === hotelRoom.number) {
+          total += hotelRoom.costPerNight
         }
       })
       return total
     }, 0)
   }
 
-  // add booking method when I create hotel and book classes
+  makeBooking(booking) {
+    this.bookings.push(booking)
+  }
 }
 export default Customer
