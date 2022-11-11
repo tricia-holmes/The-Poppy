391597b6749f8ccf3a05809d727b420b2fc3d2b9
Tricia Holmes <tricia.michele.holmes@gmail.com>
2022-11-10 19:29:34 -0600

    Add test sample data

---
+++ test-data/booking-sample-data.js
@@ -0,0 +1,32 @@
+export const bookingSampleData = [
+  {
+    id: '5fwrgu4i7k55hl6tm',
+    userID: 41,
+    date: '2022/11/10',
+    roomNumber: 19,
+  },
+  {
+    id: '5fwrgu4i7k55hl6um',
+    userID: 41,
+    date: '2021/02/07',
+    roomNumber: 4,
+  },
+  {
+    id: '5fo6gu4i7k55hl6um',
+    userID: 41,
+    date: '2023/10/07',
+    roomNumber: 4,
+  },
+  {
+    id: '5fwrgu4i7k55hl6xm',
+    userID: 23,
+    date: '2023/12/16',
+    roomNumber: 4,
+  },
+  {
+    id: '5fwrgu4i7k55hl6t7',
+    userID: 20,
+    date: '2022/11/10',
+    roomNumber: 7,
+  },
+]
---
+++ test-data/room-sample-data.js
@@ -0,0 +1,34 @@
+export const roomSampleData = [
+  {
+    number: 4,
+    roomType: 'single room',
+    bidet: false,
+    bedSize: 'queen',
+    numBeds: 1,
+    costPerNight: 429.44,
+  },
+  {
+    number: 19,
+    roomType: 'single room',
+    bidet: false,
+    bedSize: 'queen',
+    numBeds: 1,
+    costPerNight: 374.67,
+  },
+  {
+    number: 7,
+    roomType: 'single room',
+    bidet: false,
+    bedSize: 'queen',
+    numBeds: 2,
+    costPerNight: 231.46,
+  },
+  {
+    number: 24,
+    roomType: 'suite',
+    bidet: false,
+    bedSize: 'queen',
+    numBeds: 1,
+    costPerNight: 327.24,
+  },
+]
